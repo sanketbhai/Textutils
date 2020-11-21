14b113de485cbece4e1988fa76faeba8db91e50f
sanket <gsanket0903@gmail.com>
2020-11-21 23:18:16 +0530

    exersise to diasplay txt file content in browser'

--- textaile/views.py
+++ textaile/views.py
@@ -1,1 +1,11 @@
-# i created this file -sanket
+# i created this file -sanket 
+
+from django.http import HttpResponse
+
+def index(request):
+    f=open('1.txt','r')
+    
+    return HttpResponse(f.read())
+    
+def about(request):
+    return HttpResponse('<h1> this is about page </h1>')
