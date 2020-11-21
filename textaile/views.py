fb616d0deb1f4039d5ce9728121ebc5398af63c6
sanket <gsanket0903@gmail.com>
2020-11-21 23:25:50 +0530

    personal navigator exersise is completed when you go to about page you will see the link of harry bhais django play list

--- textaile/views.py
+++ textaile/views.py
@@ -8,4 +8,4 @@ def index(request):
     return HttpResponse(f.read())
     
 def about(request):
-    return HttpResponse('<h1> this is about page </h1>')
+    return HttpResponse('''<h1> this is about page </h1><br> <a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'> code with harry django play list</a> <br> <button><a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'>same link but in button</a></button>''')
