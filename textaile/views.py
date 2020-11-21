9f0c0946db25d05873da5b8fea7e30727c815f48
sanket <gsanket0903@gmail.com>
2020-11-21 23:51:19 +0530

    added some comments

--- textaile/views.py
+++ textaile/views.py
@@ -1,7 +1,18 @@
 # i created this file -sanket 
 
 from django.http import HttpResponse
-
+# code of video 6 exersise personal navigator
+#def index(request):
+    #f=open('1.txt','r')
+    
+    #return HttpResponse(f.read())
+    
+#def about(request):
+   # return HttpResponse('''<h1> this is about page </h1><br> <a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'> code with harry django play list</a> <br> <button><a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'>same link but in button</a></button>''')
+    
+    
+    
+    
 def index(request):
     f=open('1.txt','r')
     
