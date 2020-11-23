67a9110afbab7e186ce2a4c2d227ea6eebfd3e70
sanket <gsanket0903@gmail.com>
2020-11-23 23:43:07 +0530

    used post request insted of get

--- textaile/views.py
+++ textaile/views.py
@@ -1,51 +1,64 @@
 # i created this file -sanket 
 
 from django.http import HttpResponse
-# code of video 6 exersise personal navigator
-#def index(request):
-    #f=open('1.txt','r')
-    
-    #return HttpResponse(f.read())
-    
-#def about(request):
-   # return HttpResponse('''<h1> this is about page </h1><br> <a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'> code with harry django play list</a> <br> <button><a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'>same link but in button</a></button>''')
-    
-    
-    
-  # code of video 6 exersise display text file content in html 
-#def index(request):
-    #f=open('1.txt','r')
-    
-   # return HttpResponse(f.read())
-   
-#def about(request):
-    #return HttpResponse('''<h1> this is about page </h1><br> <a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'> code with harry django play list</a> <br> <button><a href='https://www.youtube.com/playlist?list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9'>same link but in button</a></button>''')
-    
-    
-    
-    
+from django.shortcuts import render 
+
 def index(request):
     
-    return HttpResponse('''home<br> <ol> <li><a href='/removepunk'>removepunk</a></li> <li><a href='/capitalizefirst'>capitalizefirst</a></li> <li><a href='/newlinermv'>newlinermv</a></li></ol>''')
-   
-def removepunk(request):
+    return render(request,'index.html')
     
-    return HttpResponse('''removepunk <a href='/'> </a> ''')
+ 
+     
    
-def capitalizefirst(request):
+def filter(request):
     
-    return HttpResponse('''<h1>capitalizefirst <a href='/'> back</a> </h1>''')
-   
-def newlinermv(request):
+    length=''
+    djtext=request.POST.get('text','default')
     
-    return HttpResponse('''<h1>newlinermv <a href='/'> back</a> </h1>''')
-   
-def spacermv(request):
+    capitalize=request.POST.get('capitalize','off')
+    rmvpunctuation=request.POST.get('rmvpunctuation','off')
+    newlineremove=request.POST.get('newlineremove','off')
+    charcount=request.POST.get('charcount','off')
+    spacermv=request.POST.get('spacermv','off')
     
-    return HttpResponse('''spacermv <a href='/'> back</a>''')
-
-def charcount(request):
+    if capitalize=='on':
+        c=''
+        for char in djtext:
+            c=c+char.upper()
+        djtext=c
+        
+    if rmvpunctuation=='on':
+        c=''
+        pun=',/;:!-_@#$&\<>][}{~`|\><'
+        for char  in djtext:
+            if char not in pun:
+                c=c+char
+        djtext=c
     
-    return HttpResponse('''charcount <a href='/'>back</a>''')
-   
-   
+    if newlineremove=='on':
+        c=''
+        for char in djtext:
+            if char!='\n' and char!='\r':
+                c+=char
+        djtext=c
+        
+    if charcount=='on':
+        length=str(len(djtext))
+    
+        
+    if spacermv=='on':
+        c=''
+        for index ,char in enumerate(djtext):
+            
+            if not(djtext[index]==' ' and djtext[index+1]==" "):
+                
+                c+=char
+            
+        
+        djtext=c
+                
+    dic ={'analizedtext':djtext,
+            'length':length}
+            
+        
+    return render(request,'analized.html',dic)
